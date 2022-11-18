976d43f4835dbbafbbc5c3ba4dfbfe8f6fc4846b
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-11-18 23:40:14 +0530

    data is render

--- index.js
+++ index.js
@@ -6,36 +6,56 @@ const { JSDOM } = jsdom
 // const axios = require('axios')
 const app = express()
 
+app.set('view engine', 'ejs')
 
-const URL = 'https://www.amazon.in/s?k=iphone+14+pro+max+phone&sprefix=iphone+14+pro+max+ph%2Caps%2C316&ref=nb_sb_ss_softlines-tsdoa-joint-contextual-iss_1_20'
 
-//s-card-container  s-image
-//s-card-containe h2 span
-//s - card - containe   a - price - whole
+const URL = 'https://www.amazon.in/s?k=iphone+14+pro+max+phone&sprefix=iphone+14+pro+max+ph%2Caps%2C638&ref=nb_sb_ss_softlines-tsdoa-joint-contextual-iss_1_20'
+
 
 async function fetchData() {
     try {
         const response = await axios.get(URL, {
             method: "GET",
-            hearders: {
+            headers: {
+                "method": " GET",
                 "authority": " www.amazon.in",
                 "upgrade-insecure-requests": 1,
                 "User- Agent": " Mozilla/ 5.0(Linux; Android 6.0; Nexus 5 Build / MRA58N) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 107.0.0.0 Mobile Safari / 537.36",
                 " Accept": " text / html, */*",
-                "path ": " /hz/rhf ? currentPageType = Search & currentSubPageType=List & excludeAsin=& fieldKeywords=& k=iphone % 252014 % 2520pro % 2520max % 2520 % 252B & keywords=& search=& auditEnabled=& previewCampaigns=& forceWidgets=& searchAlias=aps & isAUI=1 & cardJSPresent=true & pageUrl=% 2F"
+
             }
 
         })
         const { document } = (new JSDOM(response.data)).window
 
-        console.log(document.querySelector(".s-card-container .s-image").getAttribute("src"));
+        const products = []
+
+        document.querySelectorAll(".s-card-container ").forEach(element => {
+            products.push({
+                image: element.querySelector(".s-image").src,
+                title: document.querySelector(" h2 span").textContent,
+                price: document.querySelector(".a-price-whole").textContent
+            })
+        })
+        return products
 
     } catch (error) {
         console.log(error, '====error');
     }
 
 }
-fetchData()
+
+
+app.get('/', async (req, res) => {
+    const products = await fetchData()
+    // res.send(products)
+    res.render('pages/file.ejs', { products })
+})
+
+
+
+
+
 // console.log(fetchData());
 // const dom = new JSDOM('html')
 
